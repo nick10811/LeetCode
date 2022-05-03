9a4c0b611ee70cccbcde8db2ab22507d7d72055d
Nick Yang <nick10811@gmail.com>
2022-05-03 11:24:46 +0800

    [Swift] 1. Two Sum

---
+++ Swift.playground/Pages/1. Two Sum.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                if nums[j] == target - nums[i] {
+                    return [i, j]
+                }
+            }
+        }
+        return []
+    }
+}
+
+let sol = Solution()
+print(sol.twoSum([2,7,11,15], 9))
+print(sol.twoSum([3,2,4], 6))
+print(sol.twoSum([3,3], 6))
