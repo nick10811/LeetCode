68803580c9288e20848b854ebcd4c0dbb7d270aa
Nick Yang <nick10811@gmail.com>
2022-05-03 13:16:35 +0800

    [Swift] 896. Monotonic Array

---
+++ Swift.playground/Pages/896. Monotonic Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+class Solution {
+    func isMonotonic(_ nums: [Int]) -> Bool {
+        var isIncreasing: Bool? = nil
+        var preNum = 0
+        
+        for i in 0..<nums.count {
+            let num = nums[i]
+            
+            if i == 0 {
+                preNum = num
+                continue
+            }
+            
+            if let isIncreasing = isIncreasing {
+                if isIncreasing {
+                    if num < preNum {
+                        return false
+                    }
+                } else {
+                    if num > preNum {
+                        return false
+                    }
+                }
+            } else if i > 0 {
+                switch num {
+                case _ where num > preNum: isIncreasing = true
+                case _ where num < preNum: isIncreasing = false
+                default: isIncreasing = nil
+                }
+            }
+            
+            preNum = num
+        }
+        
+        return true
+    }
+}
+
+let sol = Solution()
+print(sol.isMonotonic([1,2,2,3]))
+print(sol.isMonotonic([6,5,4,4]))
+print(sol.isMonotonic([1,3,2]))
+print(sol.isMonotonic([1,1,2]))
+print(sol.isMonotonic([2,2,1]))
